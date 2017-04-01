2627d5527199ba4b09b0ff0af2c1e8b2e76a7e9d
Brian Shee <brian9shee@gmail.com>
2017-03-31 19:20:09 -0700

    Add parsing for aexpTerm

--- src/imp_parser.lua
+++ src/imp_parser.lua
@@ -10,6 +10,35 @@ end
 
 M.id = pa.Tag(il.ID)
 
-M.num = pa.Process(pa.Tag, function(i) return tonumber(i) end)
+M.num = pa.Process(pa.Tag, tonumber) 
+  --function(i) return tonumber(i) end)
+
+M.aexpValue = function()
+  return pa.Alternate(
+    pa.Process(M.num, function(i) return ex.IntAexp(i) end),
+    pa.Process(M.id, function(v) return ex.VarAexp(v) end)
+  )
+end
+
+-- TODO: This could be local?
+M.processGroup = function(parsed)
+  -- Destructure parsed from the Concat result.
+  inner = parsed[1]
+  return inner[2]
+end
+
+M.aexpGroup = function()
+  return pa.Process( 
+    pa.Concat(
+      pa.Concat(M.keyword("("), pa.Lazy(aexp)),
+      M.keyword(")")
+    ),
+    M.processGroup
+  )
+end
+
+M.aexpTerm = function()
+  return pa.Alternate(aexpValue(), aexpGroup())
+end
 
 return M
